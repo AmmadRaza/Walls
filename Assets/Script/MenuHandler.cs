d4b1aee89bb398c220f21730ff3c4c0fe8803e3d
Anas bitar <a.bitar@saedubai.com>
2016-06-18 16:50:29 +0400

    some menus added

---
+++ Assets/Script/MenuHandler.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuHandler : MonoBehaviour {
+
+	public GameObject OptionsCanvas;
+	private bool isShowing;
+
+
+
+	public void MainMenuButtonsHandler ( string ChangeMenuTo)
+	{
+		Application.LoadLevel(ChangeMenuTo);
+	}
+
+	public void QuitGame()
+	{
+
+		if ( UnityEditor.EditorApplication.isPlaying == true)
+			UnityEditor.EditorApplication.isPlaying = false;
+
+		else
+			Application.Quit();
+	}
+
+
+
+	public  void CheckForPause()
+	{
+
+		isShowing = !isShowing;
+		OptionsCanvas.SetActive(isShowing);
+
+
+	}
+}
