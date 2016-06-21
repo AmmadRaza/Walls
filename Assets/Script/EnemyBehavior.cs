6079ff52565607e70eb7c55e00b78c5fb856965b
Abdulrahman <Abdulrahman>
2016-06-21 09:57:04 +0400

    Added enemy movement script. Fixed camera position.

---
+++ Assets/Script/EnemyBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    public float speed;
+    Vector3 tempPosition;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        tempPosition = transform.position;
+        tempPosition.y += speed * Time.deltaTime;
+        transform.position = tempPosition;
+	}
+
+    /*void OnCollisionEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }*/
+}
--- Assets/Script/WaveSystem.cs
+++ Assets/Script/WaveSystem.cs
@@ -83,5 +83,4 @@ public class WaveSystem : MonoBehaviour
 	{
 		Application.LoadLevel(LoadScene);
 	}
-
 }
