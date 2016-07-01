b860039208b6ecf40eb4a4f408ace8222b131337
Abdulrahman <Abdulrahman>
2016-07-01 16:36:43 +0400

    Added 1. Game Over Menu 2. Pause Menu 3. Game Over Trigger All the UI Symbols, i made them exceot for the Repeat Arrow

---
+++ Assets/Script/GameOver.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameOver : MonoBehaviour {
+
+	public Image GameOverImage;
+
+	public Color GameColor, GameOverColor, GameOverTextColor, GameOverButtonsColor;
+
+	public float ColorChangeSpeed;
+
+	bool IsGameOver;
+
+	public Text GameOverText, ScoreText, BestScoreText;
+
+	public Button RetryButton, MenuButton;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Time.timeScale = 1;
+		IsGameOver = false;
+		GameOverImage.color = GameColor;
+		GameOverImage.gameObject.SetActive (false);
+		RetryButton.gameObject.SetActive (false);
+		MenuButton.gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (IsGameOver) 
+		{
+			Time.timeScale = 0;
+			GameOverImage.gameObject.SetActive (true);
+			GameOverImage.color = Color.Lerp (GameOverImage.color, GameOverColor, ColorChangeSpeed);
+			GameOverText.color = Color.Lerp (GameOverText.color, GameOverTextColor, ColorChangeSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.Space)) 
+		{
+			IsGameOver = true;
+		}
+		if (GameOverImage.color.a >= 0.6f) 
+		{
+			ScoreText.color = Color.Lerp (ScoreText.color, GameOverButtonsColor, ColorChangeSpeed);
+			BestScoreText.color = Color.Lerp (BestScoreText.color, GameOverButtonsColor, ColorChangeSpeed);
+		}
+		if (ScoreText.color.a >= 0.8f) 
+		{
+			RetryButton.gameObject.SetActive (true);
+			MenuButton.gameObject.SetActive (true);
+			RetryButton.image.color = Color.Lerp (RetryButton.image.color, GameOverButtonsColor, ColorChangeSpeed);
+			MenuButton.image.color = Color.Lerp (RetryButton.image.color, GameOverButtonsColor, ColorChangeSpeed);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D Col)
+	{
+		if (Col.gameObject.tag == "enemies") 
+		{
+			IsGameOver = true;
+		}
+	}
+}
---
+++ Assets/Script/PauseScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PauseScript : MonoBehaviour {
+
+	public Canvas HUDCanvas, PauseCanvas;
+
+	// Use this for initialization
+	void Start () 
+	{
+		PauseCanvas.gameObject.SetActive (false);
+		HUDCanvas.gameObject.SetActive (true);
+	}
+
+	public void OnPauseClicked()
+	{
+		Time.timeScale = 0;
+		HUDCanvas.GetComponent<GraphicRaycaster> ().enabled = false;
+		PauseCanvas.gameObject.SetActive (true);
+	}
+
+	public void OnResumeClicked()
+	{
+		PauseCanvas.gameObject.SetActive (false);
+		Time.timeScale = 1;
+		HUDCanvas.GetComponent<GraphicRaycaster> ().enabled = true;
+	}
+}
