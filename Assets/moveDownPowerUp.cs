0c3a6489ce4e53fa6e8c20c5f678ba604e86e0cf
Anas bitar <a.bitar@saedubai.com>
2016-08-01 00:52:53 +0400

    powerup is done

---
+++ Assets/moveDownPowerUp.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveDownPowerUp : MonoBehaviour {
+
+	public float speed =5;
+
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Translate(Vector3.down * speed *Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "enemies")
+			Destroy(coll.gameObject);
+			Destroy(this.gameObject , 7);
+
+	}
+}
